fef512db7ba7dae6763dc81b10cd068ec7844e9f
Jakob <jakob.fischer0408@gmail.com>
2020-09-22 11:45:19 +0200

    php updated

--- app/email.php
+++ app/email.php
@@ -1,6 +1,6 @@
 <?php
 if(isset($_POST['submit'])) {
-    $to = "jakob.fischer0408@gmail.com";
+    $to = "contact@kikoapp.at";
     $from = $_POST['email'];
     $name = $_POST['name'];
     $subject = $_POST['subject'];
