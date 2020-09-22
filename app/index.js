10f1cd8cfa11c78cc8d5ae42c7f708f599f85e6b
Alex <alre0209@gmail.com>
2020-09-22 12:53:37 +0200

    Update Javascript

--- app/index.js
+++ app/index.js
@@ -1,1 +1,11 @@
-let antrag = document.getElementById();
+document.getElementById("ansuchen").addEventListener("click", function() {
+    window.location.assign('any url or file path');
+});
+
+document.getElementById("antrag").addEventListener("click", () => {
+    window.location.assign('any url or file path');
+});
+
+document.getElementById("buch").addEventListener("click", () => {
+    window.location.assign('any url or file path');
+});
