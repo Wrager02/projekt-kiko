6041ae474b28bf27e1f62bbbca81136d3b2544f4
Jakob <jakob.fischer0408@gmail.com>
2020-09-22 12:12:32 +0200

    php updated again 2.0

--- app/email.php
+++ app/email.php
@@ -8,8 +8,8 @@ if (isset($_POST['submit'])) {
     $message = $_POST['message'];
     $message2 = "Dies ist eine Kopie Ihrer Nachricht an KIKO:\n\n\n" . $_POST['message'];
 
-    $headers = "From: " . $from . "\r\n" . "Reply-To: " . $from . "\r\n";
-    $headers2 = "From: KIKO Kontakt <contact@kikoapp.at>" . "\r\n";
+    $headers = 'From: ' . $from . "\r\n" . 'Reply-To: ' . $from . "\r\n";
+    $headers2 = 'From: KIKO Kontakt <contact@kikoapp.at>' . "\r\n";
 
     mail($to, $subject, $message, $headers);
     mail($from, $subject2, $message2, $headers2);
