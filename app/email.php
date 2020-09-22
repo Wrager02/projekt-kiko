7a3f2bc68c44e31aad0f35f122702ff06627f61b
Jakob <jakob.fischer0408@gmail.com>
2020-09-22 12:11:04 +0200

    php updated again

--- app/email.php
+++ app/email.php
@@ -1,15 +1,15 @@
 <?php
-if(isset($_POST['submit'])) {
+if (isset($_POST['submit'])) {
     $to = "contact@kikoapp.at";
     $from = $_POST['email'];
     $name = $_POST['name'];
     $subject = $_POST['subject'];
     $subject2 = "Deine Nachricht";
     $message = $_POST['message'];
-    $message2 = "Dies ist eine Kopie Ihrer Nachricht:\n\n".$_POST['message'];
+    $message2 = "Dies ist eine Kopie Ihrer Nachricht an KIKO:\n\n\n" . $_POST['message'];
 
-    $headers = "Von: " . $from;
-    $headers2 = "Von: " . $to;
+    $headers = "From: " . $from . "\r\n" . "Reply-To: " . $from . "\r\n";
+    $headers2 = "From: KIKO Kontakt <contact@kikoapp.at>" . "\r\n";
 
     mail($to, $subject, $message, $headers);
     mail($from, $subject2, $message2, $headers2);
