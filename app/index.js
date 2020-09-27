407b36f942ef4dde873c8f9162b73e8fd462db4c
Wrager02 <alre0209@gmail.com>
2020-09-27 21:19:09 +0200

    Fix burger menu in impressum and privacy pages

--- app/index.js
+++ app/index.js
@@ -9,22 +9,3 @@ document.getElementById("antrag").addEventListener("click", () => {
 document.getElementById("buch").addEventListener("click", () => {
     document.getElementById("buchDownload").click();
 });
-
-document.getElementById('hamburgerContainer').addEventListener('click', navStatus);
-document.getElementById("sliderBackground").addEventListener("click", navStatus);
-
-function navStatus() {
-    let hamburger = document.getElementById("hamburger");
-    let slider = document.getElementById("slider");
-    if (hamburger.classList.contains("hamburgerActive")) {
-        hamburger.classList.remove("hamburgerActive");
-        slider.classList.remove("sliderActive");
-        document.getElementById("sliderBackground").classList.remove("sliderActive");
-        document.getElementsByTagName("body")[0].classList.remove("disableScrolling");
-    } else {
-        hamburger.classList.add("hamburgerActive");
-        slider.classList.add("sliderActive");
-        document.getElementById("sliderBackground").classList.add("sliderActive");
-        document.getElementsByTagName("body")[0].classList.add("disableScrolling");
-    }
-}
